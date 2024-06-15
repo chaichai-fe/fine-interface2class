d7e445c5977ae626e53f5fca484800b96d511e2e
不会写React的柴柴 <75667054+chaichai-fe@users.noreply.github.com>
2024-06-15 22:48:51 +0800

    add apiRouter branch

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,16 +1,11 @@
 import { genClass } from './transform'
-import { log } from 'console'
-import { type SgNode, ts } from '@ast-grep/napi'
 
 // GET请求
 // api/code
 export async function GET(request: Request) {
-  // 获取查询参数
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
   const isSimple = searchParams.get('isSimple')
-
-  log(code, isSimple)
 
   if (code) {
     if (isSimple === 'true') {
